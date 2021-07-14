f9a814b31819c6377fb6c92a7c479599558f6fb4
tingkai <tingkai@csie.io>
2021-07-14 11:43:07 +0800

    [FIX] python request to yahoo finance need header with User-Agent

--- python/src/service/StockService.py
+++ python/src/service/StockService.py
@@ -48,7 +48,7 @@ def fetchAllStockRecord(code, start, end):
     with requests.Session() as s:
         noDataDates = []
         try:
-            download = s.get(url)
+            download = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'})
 
             decoded_content = download.content.decode('utf-8')
 
