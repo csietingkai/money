99f59e1fccaae5ffcc52982d1ab384049f02dd01
tingkai <tingkai@csie.io>
2024-07-02 22:44:17 +0800

    [DEV] prepare for auto backup

--- execute.sh
+++ execute.sh
@@ -9,7 +9,7 @@ check_tools () {
 	return 0
 }
 
-declare -a tools=( 'docker' 'git' 'mvn' 'redis-cli' 'mongodump' 'pg_dumpall' 'libpython3-dev')
+declare -a tools=( 'docker' 'git' )
 check_result=0
 for tool in "${tools[@]}" 
 do
@@ -19,7 +19,7 @@ do
 	fi
 done
 
-declare -a commands=("localhost" "build" "deploy" "backup")
+declare -a commands=("build" "backup")
 
 commit_date="$(git show -s --format=%ci --date=short)"
 version=v"$(git log -1 --pretty=format:%h)"-"$(echo $commit_date | cut -d' ' -f1 | tr "-" .)"
@@ -36,38 +36,13 @@ backend_container_name=$container_prefix-backend
 frontend_container_name=$container_prefix-frontend
 python_container_name=$container_prefix-python
 
-if [ "$1" = 'prod' ]; then
-	cd server/
-	docker-compose --project-name $project_name up -d
-	cd ../
-elif [ "$1" = 'localhost' ]; then
-	if [ "$2" = 'server' ]; then
-		cd server
-		docker-compose --project-name $project_name up -d postgres mongo redis
-		docker container ls -a
-		cd ..
-	elif [ "$2" = 'backend' ]; then
-		cd backend
-		mvn clean install package spring-boot:run
-		java -jar
-		cd ..
-	elif [ "$2" = 'frontend' ]; then
-		cd frontend
-		npm run start
-		cd ..
-	elif [ "$2" = 'python' ]; then
-		cd python
-		python PythonApplication.py
-		cd ..
-	fi
-elif [ "$1" = 'build' ]; then
+if [ "$1" = 'build' ]; then
 	if [ "$2" = 'backend' ]; then
 		cd server
 		docker container stop $backend_container_name
 		docker container rm $backend_container_name
 		cd ..
 		cd backend
-		mvn clean install package
 		docker build . --rm --tag=$backend_image_name:latest --tag=$backend_image_name:$version
 		docker push $backend_image_name:latest
 		docker push $backend_image_name:$version
@@ -105,11 +80,6 @@ else
 	echo "usage: ./execute.sh [ARGS]"
 	echo ""
 	echo "ARGS:"
-	echo -e "  prod                   start production"
-	echo -e "  localhost server       start postgresql, mongodb, redis"
-	echo -e "  localhost backend      start spring boot RESTful api"
-	echo -e "  localhost frontend     start frontend react app"
-	echo -e "  localhost python       start python app"
 	echo -e "  build backend          use docker build backend spring boot image"
 	echo -e "  build frontend         use docker build frontend react app image"
 	echo -e "  build python           use docker build python app image"
--- server/backup.sh
+++ server/backup.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-REDIS_PACKAGES=redis-tools
-MONGO_PACKAGES=mongo-tools
-POSTGRES_PACKAGES=postgresql-client-11\ postgresql-client-common
-# MYSQL_PACKAGES=mysql-client-5.7
-
-sudo apt-get install -y $REDIS_PACKAGES $MONGO_PACKAGES $POSTGRES_PACKAGES # $MYSQL_PACKAGES
-
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
 HOST=127.0.0.1
 BACKUP_DIR=./backup/$(date +%Y%m%d)
 
 if [ ! -d ./backup ]; then
 	mkdir backup
 fi
-if [ ! -d $BACKUP_DIR ]; then 
+if [ ! -d $BACKUP_DIR ]; then
 	mkdir $BACKUP_DIR
 fi
 
@@ -22,28 +15,25 @@ fi
 export $(grep -v '^#' .env | xargs -d '\n')
 
 ## backup redis
-REDIS_CLI=/usr/bin/redis-cli
-REDIS_DUMP_FILE=$BACKUP_DIR/redis-dump-$TIMESTAMP.rdb
-$REDIS_CLI -h $HOST -p $REDIS_PORT -a $REDIS_ROOT_PASSWORD --rdb $REDIS_DUMP_FILE
+REDIS_DUMP_FILE_NAME=redis-$TIMESTAMP.rdb
+REDIS_DUMP_FILE=$BACKUP_DIR/$REDIS_DUMP_FILE_NAME
+docker exec -it $CONTAINER_NAME_PREFIX-$REDIS_SERVICE redis-cli --pass $REDIS_ROOT_PASSWORD --rdb $REDIS_DUMP_FILE_NAME
+docker cp $CONTAINER_NAME_PREFIX-$REDIS_SERVICE:/data/$REDIS_DUMP_FILE_NAME $REDIS_DUMP_FILE
+docker exec -it $CONTAINER_NAME_PREFIX-$REDIS_SERVICE rm /data/$REDIS_DUMP_FILE_NAME
 echo "redis dumped"
 
 ## backup mongodb
-MONGO_CLI=/usr/bin/mongodump
-MONGO_DUMP_FILE=$BACKUP_DIR/mongo-dump-$TIMESTAMP.archive
-$MONGO_CLI -h $HOST --username $MONGODB_ROOT_USERNAME --password $MONGODB_ROOT_PASSWORD --authenticationDatabase=admin --forceTableScan --archive=$MONGO_DUMP_FILE
+MONGO_DUMP_FILE_NAME=mongo-$TIMESTAMP.gz
+MONGO_DUMP_FILE=$BACKUP_DIR/$MONGO_DUMP_FILE_NAME
+docker exec -it $CONTAINER_NAME_PREFIX-$MONGO_SERVICE mongodump --username=$MONGO_ROOT_USERNAME --password=$MONGO_ROOT_PASSWORD --authenticationDatabase=admin --db=money --archive=$MONGO_DUMP_FILE_NAME --gzip
+docker cp $CONTAINER_NAME_PREFIX-$MONGO_SERVICE:$MONGO_DUMP_FILE_NAME $MONGO_DUMP_FILE
+docker exec -it $CONTAINER_NAME_PREFIX-$MONGO_SERVICE rm $MONGO_DUMP_FILE_NAME
 echo "mongodb dumped"
 
 ## backup postgres
-POSTGRES_CLI=/usr/bin/pg_dumpall
-POSTGRES_DUMP_FILE=$BACKUP_DIR/postgres-dump-$TIMESTAMP.sql
-$POSTGRES_CLI -h $HOST -p $POSTGRES_PORT -U postgres > $POSTGRES_DUMP_FILE
+POSTGRES_DUMP_FILE=$BACKUP_DIR/postgres-$TIMESTAMP.dump
+docker exec -it $CONTAINER_NAME_PREFIX-$POSTGRES_SERVICE pg_dump --dbname=postgres --username=postgres > $POSTGRES_DUMP_FILE
 echo "postgres dumped"
-
-## backup mysql
-#MYSQL_CLI=/usr/bin/mysqldump
-#MYSQL_DUMP_FILE=$BACKUP_DIR/mysql-dump-$TIMESTAMP.sql
-#$MYSQL_CLI -h $HOST -P $MYSQL_PORT -u root -p $MYSQL_ROOT_PASSWORD --all-databases > $MYSQL_DUMP_FILE
-#echo "mysql dumped"
 
 ## delete all environment variables in .env file
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
